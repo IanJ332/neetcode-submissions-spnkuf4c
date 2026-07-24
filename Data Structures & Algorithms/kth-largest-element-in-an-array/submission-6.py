9f7f7f0dc693c3b31995de2f8cca15e0b3450c2b
Ian Jiang <100375941+IanJ332@users.noreply.github.com>
2026-07-23 20:24:23 -0700

    Add: kth-largest-element-in-an-array - submission-6

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # 1. 🧱 将原数组原地转换为小顶堆 (Min-Heap)
+        my_heap = nums
+        heapq.heapify(my_heap)
+        # 2. ✂️ 不断弹出最小值，直到堆里只剩下最大的 k 个元素
+        while len(my_heap) != k:
+            heapq.heappop(my_heap)
+
+        # 3. 🛡️ 用取负数的方式取出堆顶元素，成功避开了 abs() 对负数的错误转换
+        # [4,5]
+        negative = [-x for x in my_heap]
+        
+        return -negative[0]
